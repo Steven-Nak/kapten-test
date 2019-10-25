e83a25c11a8c2b8d51fc12e360a1a1268ca09a65
Steven <steven.nakhour@hotmail.com>
2019-10-25 16:47:30 +0200

    Handling the case if a user is already register in database

--- src/workers/loyalty/handlers/signupEvent.js
+++ src/workers/loyalty/handlers/signupEvent.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const logger = require('chpr-logger');
+const { ObjectId } = require('mongodb');
 
 const { handleMessageError } = require('../../../lib/workers');
 const riderModel = require('../../../models/riders');
@@ -15,22 +16,30 @@ const riderModel = require('../../../models/riders');
 async function handleSignupEvent(message, messageFields) {
   const { id: riderId, name } = message;
 
-  // logger.info(
-  //   { rider_id: riderId, name },
-  //   '[worker.handleSignupEvent] Received user signup event');
+  const res = await riderModel.findOneById(ObjectId(riderId));
+  if (res !== null) {
+    logger.info(
+      { rider_id: riderId, name },
+      '[worker.handleSignupEvent] Received user signup event');
+    logger.info('[worker.handleSignupEvent] Rider already treated');
+  } else {
+    logger.info(
+      { rider_id: riderId, name },
+      '[worker.handleSignupEvent] Received user signup event');
 
-  // TODO make test pass if rider already created
+    // TODO make test pass if rider already created
 
-  try {
-    // logger.info(
-    //   { rider_id: riderId, name },
-    //   '[worker.handleSignupEvent] Insert rider');
-    await riderModel.insertOne({
-      _id: riderId,
-      name
-    });
-  } catch (err) {
-    handleMessageError(err, message, messageFields);
+    try {
+      logger.info(
+        { rider_id: riderId, name },
+        '[worker.handleSignupEvent] Insert rider');
+      await riderModel.insertOne({
+        _id: riderId,
+        name
+      });
+    } catch (err) {
+      handleMessageError(err, message, messageFields);
+    }
   }
 }
 
