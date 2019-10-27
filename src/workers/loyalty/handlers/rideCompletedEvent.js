673c0ae8d6ea10aaa49a7073c05554d47c5255ef
Steven <steven.nakhour@hotmail.com>
2019-10-27 15:13:15 +0100

    Creating a ride or/and rider if not existing after a ride completed

--- src/workers/loyalty/handlers/rideCompletedEvent.js
+++ src/workers/loyalty/handlers/rideCompletedEvent.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const logger = require('chpr-logger');
 const { ObjectId } = require('mongodb');
+const logger = require('chpr-logger');
 
 const { handleMessageError } = require('../../../lib/workers');
-const riderModel = require('../../../models/riders');
+const ridesModel = require('../../../models/rides');
+const ridersModel = require('../../../models/riders');
 const loyaltyModel = require('../../../lib/loyalty');
+const date = require('../../../lib/date');
 
 /**
  * Bus message handler for ride complete events
@@ -16,19 +18,45 @@ const loyaltyModel = require('../../../lib/loyalty');
  */
 async function handleRideCompletedEvent(message) {
   const { id: rideId, amount, rider_id: riderId } = message;
-  logger.info(
-    { ride_id: rideId, rider_id: riderId, amount },
-    '[worker.handleRideCompletedEvent] Received user ride completed event');
+  const newRide = { _id: rideId, rider_id: riderId, amount };
 
   try {
-    const rider = await riderModel.findOneById(ObjectId(riderId));
-    if (!rider) throw Error('rider not exist');
+    let ride = await ridesModel.findOneById(ObjectId(rideId));
+
+    logger.info(
+      { ride_id: rideId, rider_id: riderId, amount },
+      '[worker.handleRideCompletedEvent] Received user ride completed event');
+
+    let rider = await ridersModel.findOneById(ObjectId(riderId));
+
+    if (!rider) {
+      logger.info(
+        { rider_id: riderId },
+      '[worker.handleRideCompletedEvent] Rider does not exists: insert him');
+
+      rider = await ridersModel.insertOne({ _id: riderId });
+    }
+
+    if (!ride) {
+      if (!message.create_at) newRide.created_at = date.getDate();
+      ride = await ridesModel.insertOne(newRide);
+      logger.info({ ride_id: rideId, rider_id: riderId },
+        '[worker.handleRideCompletedEvent] Insert ride');
+    }
 
     const updateInfo = await loyaltyModel.getRiderUpdate(rider, message.amount);
+    const riderUpdate = await ridersModel.updateOne(ObjectId(riderId), {
+      ride_count: updateInfo.ride_count,
+      points: updateInfo.points,
+      status: updateInfo.status
+    });
 
-    const successUpdate = await riderModel.updateOne(ObjectId(rider._id),
-      { ride_count: updateInfo.ride_count, points: updateInfo.points, status: updateInfo.status });
-    if (!successUpdate.result.nModified) throw Error('status of rider is not updated');
+    if (!riderUpdate.result.nModified) throw Error('info of rider not updated');
+
+    logger.info(
+      { ride_id: rideId, rider_id: riderId, rider_update: updateInfo },
+      '[worker.handleRideCompletedEvent] Update rider'
+    );
   } catch (err) {
     handleMessageError(err, message);
   }
