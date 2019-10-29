bf5bf14d1920997d5768a0eab34ed3c7cfa1bce4
Steven <steven.nakhour@hotmail.com>
2019-10-29 02:39:33 +0100

    Adding tests for getLoyaltyStatus

--- test/lib/riders.test.js
+++ test/lib/riders.test.js
@@ -5,6 +5,7 @@ const { ObjectId } = require('mongodb');
 
 const ridersLib = require('../../src/lib/riders');
 const riders = require('../../src/models/riders');
+const fidelity = require('../../src/models/fidelity');
 
 describe('lib/riders', () => {
   const riderId = '000000000000000000000001';
@@ -38,6 +39,39 @@ describe('lib/riders', () => {
         status: 'silver',
         points: 563,
         rides_to_next_status: 26
+      });
+    });
+  });
+
+  describe('#getLoyaltyStatus', () => {
+    it('throws if rider does not exist', async () => {
+      await fidelity.collection().remove({});
+      let error;
+      try {
+        await ridersLib.getLoyaltyStatus(riderObjectId);
+      } catch (err) {
+        error = err;
+      }
+      expect(error.message).to.equal(ridersLib.RIDER_NOT_FOUND.message);
+    });
+
+    it('returns rider loyalty status', async () => {
+      await fidelity.insertOne({
+        _id: riderId,
+        loyalty_status: {
+          bronze: { points_spent: 10, rides_count: 1 },
+          silver: { points_spent: 50, rides_count: 3 },
+          gold: { points_spent: 150, rides_count: 5 },
+          platinum: { points_spent: 250, rides_count: 15 }
+        }
+      });
+
+      const result = await ridersLib.getLoyaltyStatus(riderObjectId);
+      expect(result).to.deep.equal({
+        bronze: { points_spent: 10, rides_count: 1 },
+        silver: { points_spent: 50, rides_count: 3 },
+        gold: { points_spent: 150, rides_count: 5 },
+        platinum: { points_spent: 250, rides_count: 15 }
       });
     });
   });
