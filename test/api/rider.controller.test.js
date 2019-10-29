2fd555cc701564e1e7e88a6cd5711bf41abe8392
Steven <steven.nakhour@hotmail.com>
2019-10-29 02:39:44 +0100

    Add tests for loyalty controller

--- test/api/rider.controller.test.js
+++ test/api/rider.controller.test.js
@@ -8,6 +8,7 @@ const { ObjectId } = require('mongodb');
 const { start, stop } = require('../../src/app');
 const ridersLib = require('../../src/lib/riders');
 const riders = require('../../src/models/riders');
+const loyalty = require('../../src/models/fidelity');
 
 describe('api/rider', () => {
   const sandbox = sinon.sandbox.create();
@@ -75,6 +76,62 @@ describe('api/rider', () => {
           points: 150,
           status: 'silver',
           rides_to_next_status: 20
+        }
+      });
+    });
+  });
+
+  describe('GET /loyalty/status/:id', () => {
+    it('returns 400 if loyalty status id is invalid', async () => {
+      const { body, status } = await request(app)
+        .get('/api/rider/loyalty/status/invalid_id');
+
+      expect({ body, status }).to.deep.equal({ body: {}, status: 400 });
+    });
+
+    it('returns 404 if loyalty status is not found', async () => {
+      await loyalty.collection().remove({});
+      const { body, status } = await request(app)
+        .get(`/api/rider/loyalty/status/${riderId}`);
+
+      expect({ body, status }).to.deep.equal({ body: {}, status: 404 });
+    });
+
+    it('returns 500 if there is an unexpected error while fetching data', async () => {
+      const unexpectedError = new Error('Unexpected error');
+      const getLoyaltyStatusStub = sandbox.stub(ridersLib, 'getLoyaltyStatus')
+        .rejects(unexpectedError);
+
+      const { body, status } = await request(app)
+        .get(`/api/rider/loyalty/status/${riderId}`);
+
+      expect({ body, status }).to.deep.equal({ body: {}, status: 500 });
+
+      expect(getLoyaltyStatusStub.args).to.deep.equal([
+        [ObjectId.createFromHexString('000000000000000000000001')]
+      ]);
+    });
+
+    it('returns loyalty status', async () => {
+      await loyalty.insertOne({
+        _id: riderId,
+        loyalty_status: {
+          bronze: { points_spent: 0, rides_count: 0 },
+          silver: { points_spent: 1, rides_count: 1 },
+          gold: { points_spent: 20, rides_count: 1 },
+          platinum: { points_spent: 150, rides_count: 5 }
+        }
+      });
+
+      const { body, status } = await request(app)
+        .get(`/api/rider/loyalty/status/${riderId}`);
+
+      expect({ body, status }).to.deep.equal({
+        status: 200,
+        body: {
+          silver: { points_spent: 1, rides_count: 1 },
+          gold: { points_spent: 20, rides_count: 1 },
+          platinum: { points_spent: 150, rides_count: 5 }
         }
       });
     });
